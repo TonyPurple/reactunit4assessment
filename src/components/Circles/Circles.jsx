fd40d79824615c3271697b7f3efe2f828b48cce0
TonyPurple <tony.vanoni@outlook.com>
2022-03-26 08:15:08 -0700

    stub up 4 divs on Circles component

--- src/components/Circles/Circles.jsx
+++ src/components/Circles/Circles.jsx
@@ -3,7 +3,12 @@ import './Circles.css'
 
 function circles(props) {
     return (
-        <div className="Circles"></div>
+        <div className="Circles">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
     )
 }
 export default circles;
